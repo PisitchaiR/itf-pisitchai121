2a3f24c8c73ebaa22c4e1f1b9ae8bf8066424718
PisitchaiR <73678503+PisitchaiR@users.noreply.github.com>
2020-10-30 12:15:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gags</title>
+    <style>
+        img{
+            height: 400px;
+            width: 700px
+        }
+    </style>
 </head>
 
 <body>
