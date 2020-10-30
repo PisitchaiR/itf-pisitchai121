ba4f830c19d9af079eb815bdb02030464e074b75
PisitchaiR <73678503+PisitchaiR@users.noreply.github.com>
2020-10-30 12:14:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
     <h1>Welcome to joke</h1>
     <p>พิสิฐไชย เรืองวัฒนาพงษ์ เอ็ม</p>
     <img src="https://us-fbcloud.net/wb/data/1197/1197016-img.u3yxjy.qh2k.jpg" alt="">
-    <img src="https://scontent.fbkk2-6.fna.fbcdn.net/v/t1.0-9/120331222_3801937939835610_1230451190141094872_o.jpg?_nc_cat=104&ccb=2&_nc_sid=8bfeb9&_nc_eui2=AeE_ilOhVxduH-ZBeEAleNe920B4X4s_HpzbQHhfiz8enOnL6_hsKJGllhsBXufrTUV73sdR2T0bzt2Imgw5r4C5&_nc_ohc=RuSyvz74oTkAX80G8a4&_nc_ht=scontent.fbkk2-6.fna&oh=fa0d83fe1c6e8e171b4ccc0e2a802b54&oe=5FBF7EDC" alt="">
+    <img src="http://img.painaidii.com/images/20150521_3_1432190125_464621.jpg" alt="">
+    
 </body>
 
 </html>
