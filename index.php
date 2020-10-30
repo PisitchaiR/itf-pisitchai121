c1f4f618b3e98735cef0eb79c3dd1bbf514c2910
PisitchaiR <73678503+PisitchaiR@users.noreply.github.com>
2020-10-30 12:29:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,12 +15,14 @@
             border: 1px solid rgb(41, 41, 41);
             color: white;
             background-color: rgb(41, 41, 41);
+            padding:10px;
         }
         a:hover {
             text-decoration: none;
             border: 1px solid rgb(41, 41, 41);
             color: white;
             background-color: rgb(179, 241, 236);
+            padding:10px;
         }
     </style>
 </head>
@@ -30,7 +32,8 @@
     <p>พิสิฐไชย เรืองวัฒนาพงษ์ เอ็ม</p>
     <img src="https://us-fbcloud.net/wb/data/1197/1197016-img.u3yxjy.qh2k.jpg" alt="">
     <img src="http://img.painaidii.com/images/20150521_3_1432190125_464621.jpg" alt="">
-    <a href="http://www.youtube.com"></a>
+    <br>
+    <a href="http://www.youtube.com">YOUTUBE</a>
     
 </body>
 
