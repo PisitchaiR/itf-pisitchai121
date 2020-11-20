a46f3086d24c55dae9a87b762e9b93fc1dc2510b
PisitchaiR <73678503+PisitchaiR@users.noreply.github.com>
2020-11-21 01:57:41 +0700

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'dbitf01.mysql.database.azure.com', 'pisitchai@dbitf01', '31143114Kung', 'ITFLAB', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['Name'];
+$comment = $_POST['Comment'];
+$link = $_POST['Link'];
+
+
+$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
---
+++ update.php
@@ -0,0 +1,15 @@
+<?php
+$id=$_GET['ID'];
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'dbitf01.mysql.database.azure.com', 'pisitchai@dbitf01', '31143114Kung', 'ITFLAB', 3306);
+$name=$_POST['Name'];
+$comment=$_POST['Comment'];
+$link=$_POST['Link'];
+$sql="UPDATE Guestbook SET Name='$name',Comment='$comment',Link='$link' WHERE ID='$id'";
+if (mysqli_query($conn, $sql)) {
+    header("Location: show.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+mysqli_close($conn);
+?>
