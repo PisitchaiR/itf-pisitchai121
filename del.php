b987775f90f29c69b1d9509d1f87f0cd3ced332c
PisitchaiR <73678503+PisitchaiR@users.noreply.github.com>
2020-11-21 02:03:13 +0700

    Update del.php

--- del.php
+++ del.php
@@ -1,7 +1,7 @@
 <?php
 $id=$_GET['ID'];
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'dbitf01.mysql.database.azure.com', 'pisitchai@dbitf01', '31143114Kung', 'ITFLAB', 3306);
+mysqli_real_connect($conn, 'dbitf01.mysql.database.azure.com', 'pisitchai@dbitf01', '3114Kung', 'ITFLAB', 3306);
 $sql="DELETE FROM Guestbook WHERE ID='$id'";
 if (mysqli_connect_errno($conn))
 {
