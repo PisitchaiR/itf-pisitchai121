98be58694e270ed58f3c7bb00c44fd70d0702d26
PisitchaiR <73678503+PisitchaiR@users.noreply.github.com>
2020-11-21 02:11:06 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'dbitf01.mysql.database.azure.com', 'pisitchai@dbitf01', '3114Kung', 'ITFLAB', 3306);
+mysqli_real_connect($conn, 'dbitf01.mysql.database.azure.com', 'pisitchai@dbitf01', '31143114Kung', 'ITFLAB', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
@@ -18,7 +18,9 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
-  } else {
+    header("Location: show.php");
+  } 
+	else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
   
